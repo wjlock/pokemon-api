a17e1178f2870f8e66535dcd141dc70bf05acd81
Will Locklier <will.locklier@gmail.com>
2020-10-28 11:36:28 -0400

    [github] Update github API

--- github.js
+++ github.js
@@ -1,11 +1,24 @@
+const container = document.querySelector('.container');
+
+
+
 fetch('https://api.github.com/users/wjlock')
 .then(response => {
     console.log(response)
     return response.json();
 })
 .then(githubData => {
-    console.log(githubData)
+    // console.log(githubData);
+    const willObj = {
+        bio: githubData.bio,
+        username: githubData.login,
+        name: githubData.name
+    }
+    console.log(willObj);
+    const newElement = document.createElement('p');
+    newElement.textContent = willObj.name
+    container.appendChild(newElement);
 })
 .catch(error => {
     console.log(error)
-})
+});
